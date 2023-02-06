81e4bac0b63c583cca5b07196eb5067f3e7d615a
SondreTN <blazefigth89@gmail.com>
2023-02-06 13:00:22 +0100

    første push

---
+++ KoeT/Test/no/hvl/dat102/adt/KoeADTTest.java
@@ -0,0 +1,20 @@
+package no.hvl.dat102.adt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import no.hvl.dat102.exceptions.EmptyCollectionException;
+import no.hvl.dat102.adt.KoeADT;
+
+public abstract class KoeADTTest {
+	
+
+}
