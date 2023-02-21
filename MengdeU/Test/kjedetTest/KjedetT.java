76b712d4f98a88f9f9ddd64b74c984b8a283078f
gorildkrabbedal <gorildkrabbedal@158-37-228-49.hvl.no>
2023-02-21 12:56:38 +0100

    Merge branch 'master' of https://github.com/h589683-2/dat102obliger

---
+++ MengdeU/Test/kjedetTest/KjedetT.java
@@ -0,0 +1,14 @@
+package kjedetTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KjedetT {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ MengdeU/Test/tabellTest/TabellT.java
@@ -0,0 +1,14 @@
+package tabellTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TabellT {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
