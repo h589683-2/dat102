8a1d8d7e4c54fad14df18c1aa4d72d0827232d9f
gorildkrabbedal <gorildkrabbedal@10.0.0.10>
2023-02-28 15:43:16 +0100

    Lagt klar metoder i datakontakt

---
+++ MengdeU/hobby/hobbyProsjekt/Datakontakt.java
@@ -0,0 +1,26 @@
+package hobbyProsjekt;
+
+public class Datakontakt {
+	
+	private Medlem [] medlemTab;
+	private int antallMedlemer;
+	
+	public void leggTilMedlem(Medlem person){
+		
+	}
+	
+	public int finnMedlemIndeks(String navn) {
+		
+		return antallMedlemer;
+		
+	}
+	
+	public void finnPartnerFor(String navn) {
+		
+	}
+	
+	public void tilbakestillStatusIndeks() {
+		
+	}
+	
+}
--- MengdeU/hobby/hobbyProsjekt/Medlem.java
+++ MengdeU/hobby/hobbyProsjekt/Medlem.java
@@ -46,5 +46,6 @@ public class Medlem {
 			return false;
 		}
 		
+		
 
 }
